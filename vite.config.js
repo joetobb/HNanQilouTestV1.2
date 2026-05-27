b01e7b90b070b9574aff0ac1998f92711c8be123
Joemi <1843510043@qq.com>
2026-05-27 18:50:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // 👇 必须和你的仓库名完全一致，带斜杠
+  base: '/HNanQilouTestV1.2/',
   resolve: {
     alias: {
       cesium: 'cesium/Build/Cesium'
     }
   },
   define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium')
+    CESIUM_BASE_URL: JSON.stringify('/HNanQilouTestV1.2/cesium')
   }
 })
